7707d2f3cb0cdd115d09fa8aee605402a75fe988
juanbono <juanbono94@gmail.com>
2017-02-11 20:11:04 -0300

    downgrade del angular-cli gg

--- client/src/app/app.exports.ts
+++ client/src/app/app.exports.ts
@@ -1,5 +1,4 @@
 
-import {compose} from "@ngrx/core";
 import {storeFreeze} from "ngrx-store-freeze";
 import {localStorageSync} from "ngrx-store-localstorage";
 import {combineReducers, StoreModule} from "@ngrx/store";
@@ -13,16 +12,38 @@ import {RVEffectService} from "./shared/store/effects/rv-effects.service";
 import {PostEffectService} from "./shared/store/effects/posts-effects.service";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {INITIAL_APP_STATE} from "./shared/store/state/application.state";
+import {compose} from "@ngrx/core/compose";
+
+export function myCompose(...args: any[]) {
+  let functions:any = [];
+  for (let _i = 0; _i < args.length; _i++) {
+    functions[_i - 0] = args[_i];
+  }
+  return function (arg:any) {
+    if (functions.length === 0) {
+      return arg;
+    }
+    let last = functions[functions.length - 1];
+    let rest = functions.slice(0, -1);
+    return rest.reduceRight(function (composed:any, fn:any) { return fn(composed); }, last(arg));
+  };
+};
 
 export const ngGoogleMapsConf = AgmCoreModule.forRoot({apiKey:'AIzaSyDSA-Sc8yoe_NIGqOwTGoNPiKge0KRK_wo',libraries:['places']});
 
 export const syncedState = ['authState', 'storeData', 'router'];
-export function reducers() { return compose(storeFreeze, localStorageSync(syncedState, true), combineReducers)(appReducers); }
+//export function reducers() { return myCompose(storeFreeze, localStorageSync(syncedState, true), combineReducers)(appReducers) }
 
-export const storeConf = StoreModule.provideStore(reducers, INITIAL_APP_STATE);
+export const storeConf = StoreModule.provideStore(compose(storeFreeze, localStorageSync(syncedState, true), combineReducers)(appReducers), INITIAL_APP_STATE);
 export const routerStoreConf = RouterStoreModule.connectRouter();
 export const authEffectsConf = EffectsModule.run(AuthEffectService);
 export const userEffectsConf = EffectsModule.run(UserEffectService);
 export const rvEffectsConf = EffectsModule.run(RVEffectService);
 export const postEffectsConf = EffectsModule.run(PostEffectService);
 export const storeDevToolsConf = StoreDevtoolsModule.instrumentOnlyWithExtension();
+
+
+/*
+ "@angular/cli": "^1.0.0-beta.18",
+ "@angular/compiler-cli": "^2.4.7",
+ */
